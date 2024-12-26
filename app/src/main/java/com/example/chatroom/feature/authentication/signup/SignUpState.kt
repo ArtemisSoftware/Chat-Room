5e396a7a9221796b5b6d57002ed44c0d8830f7f5
Gustavo Isaac <gustavo.isaac@gmail.com>
2024-12-26 19:03:24 +0000

    SignUpState

---
+++ app/src/main/java/com/example/chatroom/feature/authentication/signup/SignUpState.kt
@@ -0,0 +1,12 @@
+package com.example.chatroom.feature.authentication.signup
+
+data class SignUpState(
+    val name: String = "",
+    val email: String = "",
+    val password: String = "",
+    val passwordConfirm: String = "",
+    val isLoading: Boolean = false
+) {
+    fun hasValidPasswords() = password.isNotEmpty() && passwordConfirm.isNotEmpty() && password != passwordConfirm
+    fun hasValidCredentials() = name.isNotEmpty() && email.isNotEmpty() && hasValidPasswords()
+}
