2c785a5896edbe60f4e6660744f45501e6e8764d
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-15 20:25:08 +0100

    MainActivity

--- app/src/main/java/com/example/chatroom/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/MainActivity.kt
@@ -4,22 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.chatroom.navigation.RootNavGraph
 import com.example.chatroom.ui.theme.ChatRoomTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //TODO: adicionar splash
+        /*
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.state.value.showSplash
+            }
+        }
+        */
+
         setContent {
             ChatRoomTheme {
                 val navController = rememberNavController()
-                RootNavGraph(navController = navController)
+                val state = viewModel.state.collectAsStateWithLifecycle().value
+
+                state.destinationAfterSplash?.let { route ->
+                    RootNavGraph(
+                        navController = navController,
+                        startDestination = route
+                    )
+                }
             }
         }
     }
