b375fe25e8b3d92f3bbeba3f5ebcdcccc204d5ca
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-18 17:39:31 +0100

    AuthenticationNavGraph

---
+++ app/src/main/java/com/example/chatroom/feature/authentication/navigation/AuthenticationNavGraph.kt
@@ -0,0 +1,23 @@
+package com.example.chatroom.feature.authentication.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.chatroom.feature.authentication.signin.SignInScreen
+import com.example.chatroom.feature.authentication.signup.SignUpScreen
+
+const val AUTHENTICATION_GRAPH = "authentication_graph"
+
+fun NavGraphBuilder.authenticationNavGraph(
+    navController: NavHostController,
+) {
+
+    composable<AuthenticationRoute.SignUpRoute> {
+        SignUpScreen {  }
+    }
+
+    composable<AuthenticationRoute.SignInRoute> {
+
+        SignInScreen {  }
+    }
+}
