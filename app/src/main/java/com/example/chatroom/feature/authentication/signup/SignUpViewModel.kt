cbc421f7fd8087487ff74c08c65cec833fb8fa61
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-20 19:56:19 +0100

    SignUpViewModel

---
+++ app/src/main/java/com/example/chatroom/feature/authentication/signup/SignUpViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.chatroom.feature.authentication.signup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatroom.domain.repository.AuthenticationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authenticationRepository: AuthenticationRepository
+): ViewModel() {
+
+    private val _state = MutableStateFlow(SignUpState())
+    val state = _state.asStateFlow()
+
+    fun onTriggerEvent(event: SignUpEvent){
+        when(event){
+            SignUpEvent.SignUp -> signUp()
+            is SignUpEvent.UpdateEmail -> updateEmail(event.email)
+            is SignUpEvent.UpdateName -> updateName(event.name)
+            is SignUpEvent.UpdatePassword -> updatePassword(event.password)
+            is SignUpEvent.UpdatePasswordConfirm -> updatePasswordConfirm(event.password)
+        }
+    }
+
+    private fun signUp() = with(_state.value) {
+
+        _state.update { it.copy(isLoading = true) }
+
+        viewModelScope.launch {
+            authenticationRepository
+                .signUp(name = name, email = email, password = password)
+                .onSuccess {
+                    _state.update { it.copy(isLoading = false) }
+                    //TODO: enviar evento
+                }
+                .onFailure {
+                    _state.update { it.copy(isLoading = false) }
+                    //TODO: enviar evento
+                }
+        }
+    }
+
+    private fun updateEmail(email: String) = with(_state) {
+        update { it.copy(email = email) }
+    }
+
+    private fun updateName(name: String) = with(_state) {
+        update { it.copy(name = name) }
+    }
+
+    private fun updatePassword(password: String) = with(_state) {
+        update { it.copy(password = password) }
+    }
+
+    private fun updatePasswordConfirm(password: String) = with(_state) {
+        update { it.copy(passwordConfirm = password) }
+    }
+}
