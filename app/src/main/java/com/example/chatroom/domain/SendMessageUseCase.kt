2936e9608f569ab3aa6dbacf14f0997dfd73b410
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-25 18:38:20 +0100

    SendMessageUseCase

---
+++ app/src/main/java/com/example/chatroom/domain/SendMessageUseCase.kt
@@ -0,0 +1,59 @@
+package com.example.chatroom.domain
+
+import com.example.chatroom.core.domain.Resource
+import com.example.chatroom.core.domain.error.DataError
+import com.example.chatroom.domain.repository.ChatRepository
+import com.example.chatroom.domain.repository.ImageRepository
+import javax.inject.Inject
+
+class SendMessageUseCase @Inject constructor(
+    private val chatRepository: ChatRepository,
+    private val imageRepository: ImageRepository
+) {
+    suspend operator fun invoke(
+        channelId: String,
+        text: String? = null,
+        imageUri: String? = null
+    ): Resource<Unit>{
+
+        return when{
+            imageUri != null -> {
+                sendImage(channelId = channelId, imageUri = imageUri)
+            }
+            text != null -> {
+                sendText(channelId = channelId, text = text)
+            }
+
+            else -> {
+                Resource.Failure(DataError.ChatError.MessageNotSent)
+            }
+        }
+    }
+
+    private suspend fun sendImage(channelId: String, imageUri: String): Resource<Unit>{
+        val result = imageRepository.storeImage(imageUri)
+
+        when(result){
+            is Resource.Failure -> {
+
+            }
+            is Resource.Success -> {
+                chatRepository
+                    .sendImage(channelId = channelId, image = result.data)
+            }
+        }
+
+        //TODO: provisorio
+
+        return Resource.Success(Unit)
+    }
+
+    private suspend fun sendText(channelId: String, text: String): Resource<Unit>{
+        chatRepository
+            .sendMessage(channelId = channelId, text = text)
+
+        //TODO: provisorio
+
+        return Resource.Success(Unit)
+    }
+}
