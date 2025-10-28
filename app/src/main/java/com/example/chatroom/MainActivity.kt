aae11239c20d17475d0a186141a5b006be488d0a
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-28 13:23:54 +0000

    Post notification permissions

--- app/src/main/java/com/example/chatroom/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.chatroom
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.chatroom.navigation.RootNavGraph
@@ -30,6 +35,18 @@ class MainActivity : ComponentActivity() {
                         startDestination = route
                     )
                 }
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1001
+                )
             }
         }
     }
